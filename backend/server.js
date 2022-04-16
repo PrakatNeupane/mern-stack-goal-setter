d16d4b80a3406e6fbce087bb0b4cca4a7e1c818f
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-04-16 14:02:15 +1000

    Initital REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@
 const express = require('express')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+const colors = require('colors')
+
+connectDB()
 
 const app = express()
 
